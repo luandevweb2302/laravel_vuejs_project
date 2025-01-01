18b20dbd4dedde9ba787f36e4fa0e2c46a397b01
luanvtph45976 <luanvtph45976@fpt.edu.vn>
2025-01-01 20:52:12 +0700

    setting vuetify

--- vuejs-frontend/src/main.js
+++ vuejs-frontend/src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Vuetify styles
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'; // Material Design Icons
+import '@mdi/font/css/materialdesignicons.css';
+const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+  });
+  
+  createApp(App).use(vuetify).mount('#app');
